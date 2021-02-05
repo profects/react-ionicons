dc745a6e6b9ff0bec0311040f54deb97b2b1dc91
Richard Lindhout <richardlindhout96@gmail.com>
2021-02-05 16:12:51 +0100

    Support fill color

--- src/SvgContainer.js
+++ src/SvgContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 function SvgContainer({ children, color, ...rest }){
-  return React.cloneElement(children, { ...rest, fill: color })
+  return React.cloneElement(children, { ...rest, fill: color, color })
 } 
 
 export default SvgContainer
